ef0b9abf1e8512b28c6473c538c0d93d89b3b385
chamal <chamalpriyanath457@gmail.com>
2020-09-28 14:29:36 +0530

    hellow world

---
+++ validate.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<style>
+
+div {
+  border-radius: 5px;
+  background-color: green;
+  padding: 20px;
+}
+
+</style>
+<body>
+
+<h3>Enter email to Validate</h3>
+
+<div>
+  <form action="/action_page.php">
+    
+    <input type="text" id="email" name="Email">
+    <br></br>
+
+  
+    <input type="button" value="Validate">
+  </form>
+</div>
+
+</body>
+</html>
